f956e0cbf2a26a377155747ede73373a0dad2bf1
Mathis Laliberte <mathislaliberte@gmai.com>
2023-12-03 17:38:05 -0500

    Compacting the day 1

--- 2023/day1.js
+++ 2023/day1.js
@@ -1,5 +1,4 @@
 const fs = require("fs");
-
 const result = fs.readFileSync("day1.txt", "utf-8").split("\r\n").reduce((acc, line) => {
         const cleanedLine = line.replace(/[a-zA-Z]/g, "");
         return acc + Number(cleanedLine[0] + cleanedLine[cleanedLine.length - 1]);
