d03a79116a16ed6521e0832bad0d60f47a4316cf
Mathis Laliberte <mathislaliberte@gmai.com>
2023-12-05 16:00:06 -0500

    retrait de variable inutile que j'ai oublié d'enlever

--- 2023/day1.2.js
+++ 2023/day1.2.js
@@ -1,8 +1,5 @@
 const fs = require("fs");
-const numberMapper = {
-    one: 1, two: 2, three: 3, four: 4,
-    five: 5, six: 6, seven: 7, eight: 8, nine: 9,
-};
+
 const result = fs.readFileSync("day1.txt", "utf-8").split("\r\n").map( v => {
     let words = [ 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
     for (let i in words)
@@ -11,5 +8,4 @@ const result = fs.readFileSync("day1.txt", "utf-8").split("\r\n").map( v => {
     if(digits.length > 0)
         return digits[0] + digits[digits.length - 1];
 }).map(Number).reduce((a, b) => a + b, 0);
-
 console.log(result)
