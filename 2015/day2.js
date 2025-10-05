1a8be755cd04b2b27ccbda10864998357483bc49
Mathis Laliberté <2144665@cegeplevis.ca>
2025-10-05 18:46:36 -0400

    modification of the day2

--- 2015/day2.js
+++ 2015/day2.js
@@ -1,23 +1,32 @@
-let path = 'day2.txt';
+import * as fs from "fs"
 
-let result = require("fs").readFileSync(path, "utf8").split("\r\n").map(v => v.split("x"))
+let input = fs.readFileSync("input/day2.txt", "utf8").split("\r\n").map(v => v.split("x"))
 
-let acc = 0;
-for (let line of result) {
-    let length = line[0] * line[1];
-    let width = line[1] * line[2];
-    let heigth = line[2] * line[0];
-    const min = Math.min(length, width, heigth)
-    let wrapOrder = (2 * length + 2 * width + 2 * heigth + min);
-    acc += wrapOrder;
-    console.log(acc)
+function part1(){
+    let result = 0
+    for (let line of input) {
+        let length = line[0] * line[1];
+        let width = line[1] * line[2];
+        let heigth = line[2] * line[0];
+        const min = Math.min(length, width, heigth)
+        let wrapOrder = (2 * length + 2 * width + 2 * heigth + min);
+        result += wrapOrder;
+    }
+    return result
 }
 
-//let result = require("fs").readFileSync(path, "utf8").split("\r\n").map(v => v.split("x").map(Number));
+function part2() {
+    let result = 0
+    for(let line of input){
+        line.sort((a, b) => a-b)
+        let ribbonlength = line[0] * line[1] * line[2];
+        let min = Math.min(... line)
+        let bowLength = min * 2 + line[1] * 2;
+        result += bowLength;
+        result += ribbonlength
+    }
+    return result;
+}
 
-// let acc = 0;
-// for (let line of result) {
-//     let [l, w, h] = line;
-//     const wrapOrder = 2 * (l * w + w * h + h * l) + Math.min(l * w, w * h, h * l);
-//     acc += wrapOrder;
-// }
+console.log(part1());
+console.log(part2());
