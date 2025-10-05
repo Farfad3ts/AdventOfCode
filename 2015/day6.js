b1f67501f6926f739ae20df8ec7b1feb9f348444
Mathis Laliberté <2144665@cegeplevis.ca>
2025-10-05 19:39:34 -0400

    Bit of refactoring and finished part 1

--- 2015/day6.js
+++ 2015/day6.js
@@ -1,15 +1,30 @@
-const fs = require('fs');
+import * as fs from "fs";
 
-let result = fs.readFileSync("day6.txt", "utf-8").split("\r\n").map(x => x.split(" "));
-let A = Array(1000).fill(Array(1000).fill(0));
-for(let i = 0; i < result.length; i++){
-    if(result[i][0] != "turn"){
-        let coordonate1 = result[i][1].split(',').map(Number);
-        let coordonate2 = result[i][3].split(',').map(Number);
-        
-        console.log(coordonate1, coordonate2)
-    
+const lines = fs.readFileSync("input/day6.txt", "utf-8").trim().split(/\r?\n/);
+
+function part1(){
+    const grid = Array.from({ length: 1000 }, () => Array(1000).fill(0));
+    for (let line of lines) {
+        const match = line.match(/(turn on|turn off|toggle) (\d+),(\d+) through (\d+),(\d+)/);
+        if (!match) continue;
+        const [, op, x1s, y1s, x2s, y2s] = match;
+        const x1 = Number(x1s), y1 = Number(y1s), x2 = Number(x2s), y2 = Number(y2s);
+        for (let x = x1; x <= x2; x++) {
+            for (let y = y1; y <= y2; y++) {
+                if (op === "turn on") 
+                    grid[x][y] = 1;
+                else if (op === "turn off")
+                    grid[x][y] = 0;
+                else 
+                    grid[x][y] = grid[x][y] ^ 1
+            }
+        }
     }
+    let acc = 0;
+    for (let x = 0; x < 1000; x++)
+        for (let y = 0; y < 1000; y++)
+            acc += grid[x][y];
+    return acc
 }
 
-// console.log(A)
+console.log(part1())
