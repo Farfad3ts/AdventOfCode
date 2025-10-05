d72fb077f2d2d9111872ea2078fc3a4df4883c9a
Mathis Laliberté <2144665@cegeplevis.ca>
2025-10-05 19:43:01 -0400

    Retrieved Part 2 from my old hdd and formatted the code a bit so it looked liked part 1

--- 2015/day6.js
+++ 2015/day6.js
@@ -16,7 +16,7 @@ function part1(){
                 else if (op === "turn off")
                     grid[x][y] = 0;
                 else 
-                    grid[x][y] = grid[x][y] ^ 1
+                    grid[x][y] = grid[x][y] ^ 1;
             }
         }
     }
@@ -24,7 +24,33 @@ function part1(){
     for (let x = 0; x < 1000; x++)
         for (let y = 0; y < 1000; y++)
             acc += grid[x][y];
-    return acc
+    return acc;
 }
 
-console.log(part1())
+function part2(){
+    const grid = Array.from({ length: 1000 }, () => Array(1000).fill(0));
+    for (let line of lines) {
+        const match = line.match(/(turn on|turn off|toggle) (\d+),(\d+) through (\d+),(\d+)/);
+        if (!match) continue;
+        const [, op, x1s, y1s, x2s, y2s] = match;
+        const x1 = Number(x1s), y1 = Number(y1s), x2 = Number(x2s), y2 = Number(y2s);
+        for (let x = x1; x <= x2; x++) {
+            for (let y = y1; y <= y2; y++) {
+                if (op === "turn on")
+                    grid[x][y] += 1;
+                else if (op === "turn off") 
+                    grid[x][y] = Math.max(0, grid[x][y] - 1);
+                else 
+                    grid[x][y] += 2;
+            }
+        }
+    }
+    let acc = 0;
+    for (let x = 0; x < 1000; x++)
+        for (let y = 0; y < 1000; y++)
+            acc += grid[x][y];
+    return acc;
+}
+
+console.log(part1());
+console.log(part2());
